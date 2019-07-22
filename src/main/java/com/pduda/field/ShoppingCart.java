8bf2994125ee089cc0ad342541c5bd6f650bfa67
Damián Pumar <damianpumar@gmail.com>
2019-07-22 10:14:28 +0200

    Added list of Prices all test green

--- src/main/java/com/pduda/field/ShoppingCart.java
+++ src/main/java/com/pduda/field/ShoppingCart.java
@@ -1,7 +1,15 @@
 package com.pduda.field;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
     private int price;
+    private final List<Integer> prices;
+
+    public ShoppingCart() {
+        this.prices = new ArrayList();
+    }
 
     public void add(int price) {
         this.price = price;
